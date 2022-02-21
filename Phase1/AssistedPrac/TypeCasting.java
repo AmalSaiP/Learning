89c05e74611e8402cde52e3e4e25ba47611e51d9
Amal <67532242+AmalSaiP@users.noreply.github.com>
2022-02-21 21:20:02 +0530

    Add files via upload

---
+++ Phase1/AssistedPrac/TypeCasting.java
@@ -0,0 +1,20 @@
+package com.mphasis.demo;
+
+import java.util.Scanner;
+
+public class TypeCasting {
+	public static void main(String[ ] args) {
+
+	char c = 'A';
+	int a= c;
+	System.out.println(a);
+	
+	
+	
+	Scanner s=new Scanner(System.in);
+	int v= s.nextInt();
+	char d=(char)v;
+	System.out.println(d);
+	}
+
+}

4246dee7b5a208948e5eff06a8c6ad06113b88e9
Amal <67532242+AmalSaiP@users.noreply.github.com>
2022-02-24 10:49:10 +0530

    Add files via upload

--- Phase1/AssistedPrac/Constructors.java
+++ Phase1/AssistedPrac/Constructors.java
@@ -5,38 +5,33 @@ public class Constructors {
 	public static void main(String[] args) {
 
 		EmpInfo emp1 = new EmpInfo();
-		EmpInfo emp2 = new EmpInfo();
-
 		emp1.display();
-		emp2.display();
-		
-		Para std1=new Para(2,"Abcd");
-		Para std2=new Para(4,"Efgh");
+
+		Para std1 = new Para(2, "Abcd");
 		std1.display();
-		std2.display();
 
 	}
 }
 
 class EmpInfo {
-	int id;
+	int id = 10;
 	String name;
 
 	void display() {
 		System.out.println(id + " " + name);
 	}
 }
-class Para{
+
+class Para {
 	int id;
 	String name;
 
-	Para(int i,String n)
-	{
-	id=i;
-	name=n;
+	Para(int i, String n) {
+		id = i;
+		name = n;
 	}
 
 	void display() {
-	System.out.println(id+" "+name);
+		System.out.println(id + " " + name);
 	}
 }

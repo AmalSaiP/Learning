2a0e065a2aafb1ef9e23f75a74e3d4e25382dfe1
Amal <amalsai05@gmail.com>
2022-04-01 09:20:30 +0530

    Phase 3 Assessment

--- Phase3/Assessment/Ass3/Phase3SportyShoes/src/main/java/com/ss/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ss.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ss.model.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Integer>{
-
-}
